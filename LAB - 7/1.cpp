8a479f95de5e0d2d98046a1e2adc98ea44b6051c
Hawsgard123 <92020509+Hawsgard123@users.noreply.github.com>
2022-10-15 19:58:46 +0530

    Add files via upload

--- LAB - 7/1.cpp
+++ LAB - 7/1.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+using namespace std;
 
+class A
+{
+    public:
+        A()
+        {
+            cout << "A" << " ";
+        }
+        ~A()
+        {
+            cout << "~A" << " ";
+        }
+};
+class B: public A 
+{
+    public:
+        B()
+        {
+            cout << "B" << " ";
+        }
+        ~B()
+        {
+            cout << "~B" << " ";
+        }
+};
+class C: public A 
+{
+    public:
+        C()
+        {
+            cout << "C" << " ";
+        }
+        ~C()
+        {
+            cout << "~C" << " ";
+        }
+};
+
+int main()
+{
+    B b;
+    C c;
+}
